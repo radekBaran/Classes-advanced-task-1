b526f094d87e35f57ff4f13f215abe4fedebfd92
Radoslaw Baranan <th3erm4n@gmail.com>
2016-11-12 15:05:34 +0100

    added a size enum to project

---
+++ src/Size.java
@@ -0,0 +1,19 @@
+public enum Size {
+    LARGE("Duże butelkki (2l)"),
+    MEDIUM("Średnie butelki (1l)"),
+    SMALL("Małe butelki (0.5l)");
+
+    private String description;
+
+    Size(String description) {
+        setDescription(description);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
