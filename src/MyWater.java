6bdb98833014af604b7cfcda877c037703d1fc81
Radoslaw Baranan <th3erm4n@gmail.com>
2016-11-12 15:12:02 +0100

    added a hashMap which will contains bottles and also adde a enum object called size

--- src/MyWater.java
+++ src/MyWater.java
@@ -1,3 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyWater {
+
+    private static Size size;
+    private static Map<String, Integer> bottles = new HashMap<String, Integer>();
+
     
 }
